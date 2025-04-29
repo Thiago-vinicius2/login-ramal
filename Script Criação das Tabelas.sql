7bc17dabbdbcc18ce82eaea72992d7191ea56a10
Thiago Araújo <138731393+Thiago-vinicius2@users.noreply.github.com>
2025-04-29 15:39:04 -0300

    Add files via upload

---
+++ Script Criação das Tabelas.sql
@@ -0,0 +1,14 @@
+Create TABLE ramal (
+	id INT PRIMARY KEY IDENTITY,
+	numero_ramal INT NOT NULL,
+	status_ramal VARCHAR(10) NOT NULL CHECK (status_ramal IN ('Disponivel', 'Ocupado', 'Manutencao'))
+);
+
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY IDENTITY,
+	nome VARCHAR(50) NOT NULL,
+	sobrenome VARCHAR(50) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	senha VARCHAR(50) NOT NULL
+);
