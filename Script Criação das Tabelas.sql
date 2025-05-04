fde28b4f2ada0d258ff9f9cee1b1f6db59b4987e
Thiago Araújo <138731393+Thiago-vinicius2@users.noreply.github.com>
2025-05-04 15:47:43 -0300

    Update Script Criação das Tabelas.sql

--- Script Criação das Tabelas.sql
+++ Script Criação das Tabelas.sql
@@ -1,9 +1,8 @@
-Create TABLE ramal (
+CREATE TABLE ramal (
 	id INT PRIMARY KEY IDENTITY,
 	numero_ramal INT NOT NULL,
 	status_ramal VARCHAR(10) NOT NULL CHECK (status_ramal IN ('Disponivel', 'Ocupado'))
 );
-
 
 CREATE TABLE usuario (
 	id INT PRIMARY KEY IDENTITY,
